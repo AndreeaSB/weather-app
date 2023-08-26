2d485cc1743393be1e37cf37c07cc5afebf5c340
AndreeaSB <andreea.sbadea@gmail.com>
2023-08-26 16:26:31 +0300

    message

---
+++ src/api.js
@@ -0,0 +1,12 @@
+export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo';
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '9033a051f7msh848e5b6e555072cp16329fjsn1e0a46be75ef',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+
+export const WEATHER_API_KEY = "f0e50211f2f6798325d7a3c5b63a468f";
